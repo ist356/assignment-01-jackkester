1e6078c69d569135d4c6524cba56ca66129f4918
jackkester <jwkester@syr.edu>
2024-09-07 00:30:49 -0400

    Did assignment. Changed gpa.py and wrote the code for numbers.py

--- code/numbers.py
+++ code/numbers.py
@@ -11,4 +11,27 @@ For example, if the input is:
 The output should be:
 {'odd': [3, 5], 'even': [2, 4, 6]}
 '''
+odd_list = []
+even_list = []
+while True:
+    
+    num = int(input("Input a number: "))
+    if num == 0:
+        dictionary = {'odd': odd_list, 'even': even_list}
+        print(dictionary)
+        break
+        
+    
+    if num % 2 == 0:
+        #count_even += 1
+        even_list.append(num)
+    elif num % 2 == 1:
+        #count_odd += 1
+        odd_list.append(num)
 
+
+    
+
+
+
+    
